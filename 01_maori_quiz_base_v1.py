aab23d1b331c52bed89e6d97bb5ca9fd9871de0b
Wudthedud <wud@middleton.school.nz>
2023-05-03 11:32:50 +1200

    udpate

--- 01_maori_quiz_base_v1.py
+++ 01_maori_quiz_base_v1.py
@@ -7,5 +7,5 @@ By Daniel Wu
 name = input("Welcome to the Maori Language Quiz, what is your name?")
 age = int(input(f"Hello {name}, how old are you?"))
 
-if age > 12
+if age > 12:
     print("You are too old for this game")
---
+++ 02_welcome_v1.py
@@ -0,0 +1,10 @@
+from
+
+name = input("Welcome to the Maori Language Quiz, what is your name?")
+age = int(input(f"Hello {name}, how old are you?"))
+
+if age > 12:
+    question = "You are too old for this game, would you like to continue? (Y/N)"
+    yes_no
+elif age < 5:
+    question = "You are too young for this game, would you like to continue? (Y/N)"
