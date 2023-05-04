53789ccef93a0ee4c64117e3b987a0d02447f5f7
Wudthedud <wud@middleton.school.nz>
2023-05-04 14:56:40 +1200

    Update

--- 02_welcome_v1.py
+++ 02_welcome_v1.py
@@ -1,11 +1,4 @@
-from FINAL_yes_no_checker_v4 import yes_no
-
 name = input("Welcome to the Maori Language Quiz, what is your name? \n")
 age = int(input(f"Hello {name.title()}, how old are you? \n "))
 
-if age > 12:
-    question = "You are too old for this game, would you like to continue?"
-    yes_no(question)
-elif age < 5:
-    question = "You are too young for this game, would you like to continue? (Y/N)"
-    yes_no(question)
+
