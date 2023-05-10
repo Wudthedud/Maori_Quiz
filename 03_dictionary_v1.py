270c15a6486a9d32103a98cf014d1683707651ab
Wudthedud <wud@middleton.school.nz>
2023-05-11 00:08:27 +1200

    Update

--- 03_dictionary_v1.py
+++ 03_dictionary_v1.py
@@ -15,5 +15,10 @@ dictionary_months = {1: "January",
                      4: "April",
                      5: "May",
                      6: "June",
-                     7: "July"
-print(dictionary_numbers["one"])
+                     7: "July",
+                     8: "August",
+                     9: "September",
+                     10: "October",
+                     11: "November",
+                     12: "December"}
+
