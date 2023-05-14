ebc6f62bc725f77a73bf59d16a3eb06ee2336677
Wudthedud <wud@middleton.school.nz>
2023-05-14 23:49:32 +1200

    Formatting Update

--- line checker.py
+++ line checker.py
@@ -1,11 +1,11 @@
-import shelve
+def center(string):
+    whitespace = 50 - len(string)
+    spaces = whitespace / 2
+    for i in range(int(spaces)):
+        print(" ", end="")
+    print(string, end="")
+    for i in range(int(spaces)):
+        print(" ", end="")
 
-d = shelve.open('score.txt')
-score = d['highscores_numbers']  # the score is read from disk
-d.close()
 
-print("\t High Scores:")
-for i in range(5):
-    score_ = score[i]
-    print(f"{score_[0]}          {score_[1]} points")
-
+center("hello")
